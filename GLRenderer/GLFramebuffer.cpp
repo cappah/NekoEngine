d00a6bcaf3d83a13c495f56e2084cf27855c66a6
Alexandru Naiman <alexandru.naiman@icloud.com>
2016-04-30 03:39:24 +0300

    Implemented GLFramebuffer_NoDSA

--- GLRenderer/GLFramebuffer.cpp
+++ GLRenderer/GLFramebuffer.cpp
@@ -281,146 +281,176 @@ GLFramebuffer_NoDSA::GLFramebuffer_NoDSA(int width, int height)
 
 void GLFramebuffer_NoDSA::AttachTexture(DrawAttachment attachment, class RTexture* texture)
 {
-	GL_CHECK(glNamedFramebufferTexture(_id, GL_Attachments[(int)attachment], ((GLTexture *)texture)->GetId(), 0));
+	GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, _id));
+	GL_CHECK(glFramebufferTexture(GL_FRAMEBUFFER, GL_Attachments[(int)attachment], ((GLTexture *)texture)->GetId(), 0));
 }
 
 void GLFramebuffer_NoDSA::AttachDepthTexture(class RTexture* texture)
 {
-	GL_CHECK(glNamedFramebufferTexture(_id, GL_DEPTH_ATTACHMENT, ((GLTexture *)texture)->GetId(), 0));
+	GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, _id));
+	GL_CHECK(glFramebufferTexture(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, ((GLTexture *)texture)->GetId(), 0));
 }
 
 void GLFramebuffer_NoDSA::AttachDepthStencilTexture(class RTexture* texture)
 {
-	GL_CHECK(glNamedFramebufferTexture(_id, GL_DEPTH_STENCIL_ATTACHMENT, ((GLTexture *)texture)->GetId(), 0));
+	GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, _id));
+	GL_CHECK(glFramebufferTexture(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, ((GLTexture *)texture)->GetId(), 0));
 }
 
 void GLFramebuffer_NoDSA::CreateDepthBuffer()
 {
-	GL_CHECK(glCreateRenderbuffers(1, &_rbos[RBO_DEPTH]));
-	GL_CHECK(glNamedRenderbufferStorage(_rbos[RBO_DEPTH], GL_DEPTH_COMPONENT24, _width, _height));
-	GL_CHECK(glNamedFramebufferRenderbuffer(_id, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, _rbos[RBO_DEPTH]));
+	GL_CHECK(glGenRenderbuffers(1, &_rbos[RBO_DEPTH]));
+	GL_CHECK(glBindRenderbuffer(GL_RENDERBUFFER, _rbos[RBO_DEPTH]));
+	GL_CHECK(glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, _width, _height));
+	GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, _id));
+	GL_CHECK(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, _rbos[RBO_DEPTH]));
 }
 
 void GLFramebuffer_NoDSA::CreateMultisampledDepthBuffer(int samples)
 {
-	GL_CHECK(glCreateRenderbuffers(1, &_rbos[RBO_DEPTH]));
-	GL_CHECK(glNamedRenderbufferStorageMultisample(_rbos[RBO_DEPTH], samples, GL_DEPTH_COMPONENT24, _width, _height));
-	GL_CHECK(glNamedFramebufferRenderbuffer(_id, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, _rbos[RBO_DEPTH]));
+	GL_CHECK(glGenRenderbuffers(1, &_rbos[RBO_DEPTH]));
+	GL_CHECK(glBindRenderbuffer(GL_RENDERBUFFER, _rbos[RBO_DEPTH]));
+	GL_CHECK(glRenderbufferStorageMultisample(GL_RENDERBUFFER, samples, GL_DEPTH_COMPONENT24, _width, _height));
+	GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, _id));
+	GL_CHECK(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, _rbos[RBO_DEPTH]));
 }
 
 void GLFramebuffer_NoDSA::CreateStencilBuffer()
 {
-	GL_CHECK(glCreateRenderbuffers(1, &_rbos[RBO_STENCIL]));
-	GL_CHECK(glNamedRenderbufferStorage(_rbos[RBO_STENCIL], GL_STENCIL_INDEX8, _width, _height));
-	GL_CHECK(glNamedFramebufferRenderbuffer(_id, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, _rbos[RBO_STENCIL]));
+	GL_CHECK(glGenRenderbuffers(1, &_rbos[RBO_STENCIL]));
+	GL_CHECK(glBindRenderbuffer(GL_RENDERBUFFER, _rbos[RBO_STENCIL]));
+	GL_CHECK(glRenderbufferStorage(GL_RENDERBUFFER, GL_STENCIL_INDEX8, _width, _height));
+	GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, _id));
+	GL_CHECK(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, _rbos[RBO_STENCIL]));
 }
 
 void GLFramebuffer_NoDSA::CreateMultisampledStencilBuffer(int samples)
 {
-	GL_CHECK(glCreateRenderbuffers(1, &_rbos[RBO_STENCIL]));
-	GL_CHECK(glNamedRenderbufferStorageMultisample(_rbos[RBO_STENCIL], samples, GL_STENCIL_INDEX8, _width, _height));
-	GL_CHECK(glNamedFramebufferRenderbuffer(_id, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, _rbos[RBO_STENCIL]));
+	GL_CHECK(glGenRenderbuffers(1, &_rbos[RBO_STENCIL]));
+	GL_CHECK(glBindRenderbuffer(GL_RENDERBUFFER, _rbos[RBO_STENCIL]));
+	GL_CHECK(glRenderbufferStorageMultisample(GL_RENDERBUFFER, samples, GL_STENCIL_INDEX8, _width, _height));
+	GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, _id));
+	GL_CHECK(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, _rbos[RBO_STENCIL]));
 }
 
 void GLFramebuffer_NoDSA::CreateDepthStencilBuffer()
 {
-	GL_CHECK(glCreateRenderbuffers(1, &_rbos[RBO_DEPTH_STENCIL]));
-	GL_CHECK(glNamedRenderbufferStorage(_rbos[RBO_DEPTH_STENCIL], GL_DEPTH24_STENCIL8, _width, _height));
-	GL_CHECK(glNamedFramebufferRenderbuffer(_id, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, _rbos[RBO_DEPTH_STENCIL]));
+	GL_CHECK(glGenRenderbuffers(1, &_rbos[RBO_DEPTH_STENCIL]));
+	GL_CHECK(glBindRenderbuffer(GL_RENDERBUFFER, _rbos[RBO_DEPTH_STENCIL]));
+	GL_CHECK(glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, _width, _height));
+	GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, _id));
+	GL_CHECK(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, _rbos[RBO_DEPTH_STENCIL]));
 }
 
 void GLFramebuffer_NoDSA::CreateMultisampledDepthStencilBuffer(int samples)
 {
-	GL_CHECK(glCreateRenderbuffers(1, &_rbos[RBO_DEPTH_STENCIL]));
-	GL_CHECK(glNamedRenderbufferStorageMultisample(_rbos[RBO_DEPTH_STENCIL], samples, GL_DEPTH24_STENCIL8, _width, _height));
-	GL_CHECK(glNamedFramebufferRenderbuffer(_id, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, _rbos[RBO_DEPTH_STENCIL]));
+	GL_CHECK(glGenRenderbuffers(1, &_rbos[RBO_DEPTH_STENCIL]));
+	GL_CHECK(glBindRenderbuffer(GL_RENDERBUFFER, _rbos[RBO_DEPTH_STENCIL]));
+	GL_CHECK(glRenderbufferStorageMultisample(GL_RENDERBUFFER, samples, GL_DEPTH24_STENCIL8, _width, _height));
+	GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, _id));
+	GL_CHECK(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, _rbos[RBO_DEPTH_STENCIL]));
 }
 
 FramebufferStatus GLFramebuffer_NoDSA::CheckStatus()
 {
 	GLenum status;
-	GL_CHECK(status = glCheckNamedFramebufferStatus(_id, GL_FRAMEBUFFER));
-
+	GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, _id));
+	GL_CHECK(status = glCheckFramebufferStatus(GL_FRAMEBUFFER));
+	
 	if (status == GL_FRAMEBUFFER_COMPLETE)
 		return FramebufferStatus::Complete;
-
+	
 	if (status == GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT)
 		return FramebufferStatus::IncompleteAttachment;
-
+	
 	if (status == GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT)
 		return FramebufferStatus::MissingAttachment;
-
+	
 	if (status == GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE)
 		return FramebufferStatus::IncompleteMultisample;
-
+	
 	return FramebufferStatus::Unsupported;
 }
 
 void GLFramebuffer_NoDSA::Blit(RFramebuffer* dest, int srcX0, int srcY0, int srcX1, int srcY1, int dstX0, int dstY0, int dstX1, int dstY1)
 {
 	GLuint destFbo = 0;
-
+	
 	if (dest != R_DEFAULT_FRAMEBUFFER)
 	{
 		GLFramebuffer *d = (GLFramebuffer *)dest;
 		destFbo = d->GetId();
 	}
-
-	GL_CHECK(glBlitNamedFramebuffer(_id, destFbo, srcX0, srcY0, srcX1, srcY1, dstX0, dstY0, dstX1, dstY1, GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT, GL_NEAREST));
+	
+	GL_CHECK(glBindFramebuffer(GL_DRAW_FRAMEBUFFER, destFbo));
+	GL_CHECK(glBindFramebuffer(GL_READ_FRAMEBUFFER, _id));
+	
+	GL_CHECK(glBlitFramebuffer(srcX0, srcY0, srcX1, srcY1, dstX0, dstY0, dstX1, dstY1, GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT, GL_NEAREST));
 }
 
 void GLFramebuffer_NoDSA::CopyColor(RFramebuffer* dest, TextureFilter filter)
 {
 	GLuint destFbo = 0;
-
+	
 	if (dest != R_DEFAULT_FRAMEBUFFER)
 	{
 		GLFramebuffer *d = (GLFramebuffer *)dest;
 		destFbo = d->GetId();
 	}
-
-	GL_CHECK(glBlitNamedFramebuffer(_id, destFbo, 0, 0, _width, _height, 0, 0, _width, _height, GL_COLOR_BUFFER_BIT, GL_TexFilter[(int)filter]));
+	
+	GL_CHECK(glBindFramebuffer(GL_DRAW_FRAMEBUFFER, destFbo));
+	GL_CHECK(glBindFramebuffer(GL_READ_FRAMEBUFFER, _id));
+	
+	GL_CHECK(glBlitFramebuffer(0, 0, _width, _height, 0, 0, _width, _height, GL_COLOR_BUFFER_BIT, GL_TexFilter[(int)filter]));
 }
 
 void GLFramebuffer_NoDSA::CopyDepth(RFramebuffer* dest)
 {
 	GLuint destFbo = 0;
-
+	
 	if (dest != R_DEFAULT_FRAMEBUFFER)
 	{
 		GLFramebuffer *d = (GLFramebuffer *)dest;
 		destFbo = d->GetId();
 	}
-
-	GL_CHECK(glBlitNamedFramebuffer(_id, destFbo, 0, 0, _width, _height, 0, 0, _width, _height, GL_DEPTH_BUFFER_BIT, GL_NEAREST));
+	
+	GL_CHECK(glBindFramebuffer(GL_DRAW_FRAMEBUFFER, destFbo));
+	GL_CHECK(glBindFramebuffer(GL_READ_FRAMEBUFFER, _id));
+	
+	GL_CHECK(glBlitFramebuffer(0, 0, _width, _height, 0, 0, _width, _height, GL_DEPTH_BUFFER_BIT, GL_NEAREST));
 }
 
 void GLFramebuffer_NoDSA::CopyStencil(RFramebuffer* dest)
 {
 	GLuint destFbo = 0;
-
+	
 	if (dest != R_DEFAULT_FRAMEBUFFER)
 	{
 		GLFramebuffer *d = (GLFramebuffer *)dest;
 		destFbo = d->GetId();
 	}
-
-	GL_CHECK(glBlitNamedFramebuffer(_id, destFbo, 0, 0, _width, _height, 0, 0, _width, _height, GL_STENCIL_BUFFER_BIT, GL_NEAREST));
+	
+	GL_CHECK(glBindFramebuffer(GL_DRAW_FRAMEBUFFER, destFbo));
+	GL_CHECK(glBindFramebuffer(GL_READ_FRAMEBUFFER, _id));
+	
+	GL_CHECK(glBlitFramebuffer(0, 0, _width, _height, 0, 0, _width, _height, GL_STENCIL_BUFFER_BIT, GL_NEAREST));
 }
 
 void GLFramebuffer_NoDSA::SetDrawBuffer(DrawAttachment attachment)
 {
-	GL_CHECK(glNamedFramebufferDrawBuffer(_id, GL_Attachments[(int)attachment]));
+	GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, _id));
+	GL_CHECK(glDrawBuffer(GL_Attachments[(int)attachment]));
 }
 
 void GLFramebuffer_NoDSA::SetDrawBuffers(int32_t n, DrawAttachment* buffers)
 {
 	GLenum drawBuffers[11];
-
+	
 	for (int i = 0; i < n; i++)
 		drawBuffers[i] = GL_Attachments[(int)buffers[i]];
-
-	GL_CHECK(glNamedFramebufferDrawBuffers(_id, n, drawBuffers));
+	
+	GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, _id));
+	GL_CHECK(glDrawBuffers(n, drawBuffers));
 }
 
 GLFramebuffer_NoDSA::~GLFramebuffer_NoDSA()
